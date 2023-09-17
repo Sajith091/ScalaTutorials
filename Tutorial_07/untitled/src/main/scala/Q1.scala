a333996741ee15712b12cbc67c27af1ddec4578e
D.N.S. BANDARA <136679322+Sajith091@users.noreply.github.com>
2023-09-17 20:02:50 +0530

    Delete Tutorial_07/untitled directory

--- Tutorial_07/untitled/src/main/scala/Q1.scala
+++
@@ -1,9 +0,0 @@
-object Q1 extends App{
-  def main(args: Array[String]): Unit = {
-    def filterEvenNumbers(numberList :List[Int]): List[Int] ={
-      numberList.filter(number =>number%2==0)
-    }
-    val numberList : List[Int] = List(1,2,3,4,5,6,7,8,9,10)
-    println(filterEvenNumbers(numberList))
-   }
-}
--- Tutorial_07/untitled/src/main/scala/Q2.scala
+++
@@ -1,8 +0,0 @@
-object Q2 extends App {
-  def calculateSquare(numbers: List[Int]): List[Int] = {
-    numbers.map(n => n*n)
-  }
-
-  val List2 = List(1, 2, 3, 4, 5)
-  println(calculateSquare(List2))
-}
